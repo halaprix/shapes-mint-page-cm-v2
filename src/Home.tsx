95d15cb5d59ab35250d473c9b4d69ad1c9c4d67f
Halaprix <panicz.klos@gmail.com>
2022-01-28 09:26:23 +0100

    connect button style

--- src/Home.tsx
+++ src/Home.tsx
@@ -24,14 +24,14 @@ const IMAGE_LINK = "/example.gif";
 const LOGO_LINK = "/logo_shapes_nobg.png";
 
 const ConnectButton = styled(WalletDialogButton)`
-  width: 100%;
-  height: 60px;
-  margin-top: 10px;
-  margin-bottom: 5px;
-  background: linear-gradient(180deg, #604ae5 0%, #813eee 100%);
-  color: white;
-  font-size: 16px;
-  font-weight: bold;
+width: 100%;
+height: 60px;
+margin-top: 10px;
+margin-bottom: 5px;
+background: linear-gradient(29deg, #34342F 0%,  #44C3A1 100%);
+color: white;
+font-size: 16px;
+font-weight: bold;
 `;
 
 const MintContainer = styled.div``; // add your owns styles here
